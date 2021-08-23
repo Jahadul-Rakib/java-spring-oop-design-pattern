8762b5e86ca7b53cec9f8fb8adffbcdc9e20da25
Jahadul-Rakib <rakibdiu2015@gmail.com>
2021-08-23 12:45:36 +0600

    add

--- src/main/java/com/rakib/javadesignpattern/behavioral/iterator/Application.java
+++ src/main/java/com/rakib/javadesignpattern/behavioral/iterator/Application.java
@@ -3,21 +3,11 @@ package com.rakib.javadesignpattern.behavioral.iterator;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.Iterator;
-import java.util.ListIterator;
-
 @SpringBootApplication
 public class Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
-
-        ListIterator<String> iterator = Arrays.asList("A","B","C").listIterator();
-        iterator.forEachRemaining(s -> {
-            System.out.println(s +" Current Value.");
-        });
 
     }
 }
--- src/main/java/com/rakib/javadesignpattern/behavioral/iterator/read-me.md
+++ src/main/java/com/rakib/javadesignpattern/behavioral/iterator/read-me.md
@@ -1,3 +1,5 @@
 # Iterator Pattern
 
 * ### **Its design pattern where we can store different type of collection in single container.**
+* **This pattern is used to get a way to access the elements of a collection object in sequential manner without
+  any need to know its underlying representation.**
